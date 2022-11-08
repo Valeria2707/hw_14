07bbfd2a6ebc8df193f86a7f6cd5d4f0a10d0a40
Valeria Zavadetska <Valeria.zavadetska@gmail.com>
2022-11-08 10:03:09 +0200

    start project

---
+++ js/app.js
@@ -0,0 +1,38 @@
+function showCategories() {
+  const container = document.querySelector('.categories');
+
+  for (let i = 0; i < data.length; i++) {
+    const elem = document.createElement('div');
+    elem.textContent = data[i].name;
+    elem.setAttribute('data-category', i);
+    elem.addEventListener('click', showProducts);
+    container.appendChild(elem);
+  }
+}
+
+// handler of click on categories
+function showProducts(event) {
+  const categoryIndex = event.target.getAttribute('data-category');
+  const products = data[categoryIndex].products;
+  const container = document.querySelector('.products');
+  container.innerHTML = '';
+  
+  for(let i = 0; i < products.length; i++) {
+    const elem = document.createElement('div');
+    elem.textContent = products[i].name;
+    elem.setAttribute('data-product', i);
+    elem.setAttribute('data-category', categoryIndex);
+    elem.addEventListener('click', showDetails);
+    container.appendChild(elem);
+  }
+}
+
+function showDetails(event) {
+  const categoryIndex = event.target.getAttribute('data-category');
+  const productIndex = event.target.getAttribute('data-product');
+
+  console.log(data[categoryIndex].products[productIndex]);
+
+}
+
+showCategories();
