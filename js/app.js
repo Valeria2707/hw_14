13293939dbab42f8e40527034a923c6ea6124466
Valeria Zavadetska <Valeria.zavadetska@gmail.com>
2022-11-08 12:50:03 +0200

    filling files

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ function showProducts(event) {
   const products = data[categoryIndex].products;
   const container = document.querySelector('.products');
   container.innerHTML = '';
-  
+
   for(let i = 0; i < products.length; i++) {
     const elem = document.createElement('div');
     elem.textContent = products[i].name;
@@ -30,9 +30,53 @@ function showProducts(event) {
 function showDetails(event) {
   const categoryIndex = event.target.getAttribute('data-category');
   const productIndex = event.target.getAttribute('data-product');
+  const productInf = data[categoryIndex].products[productIndex];
+  const container = document.querySelector('.details');
+  container.innerHTML='';
 
-  console.log(data[categoryIndex].products[productIndex]);
 
+  const elemName = document.createElement('div');
+  elemName.textContent = productInf.name;
+  elemName.classList.add('nameProduct');
+  container.appendChild(elemName);
+
+  const elemPrice = document.createElement('div');
+  elemPrice.textContent ="Ціна: " + productInf.price + "$";
+  container.appendChild(elemPrice);
+
+  const elemDescription = document.createElement('div');
+  elemDescription.textContent ="Опис товару: " + productInf.description;
+  container.appendChild(elemDescription);
+
+  const buttonBuy = document.createElement('button');
+  buttonBuy.classList.add('btnBuy');
+  buttonBuy.textContent = 'Купити';
+  container.appendChild(buttonBuy);
+
+  buttonBuy.addEventListener('click', showMessage);
+}
+
+function showMessage(){
+  const  elem = document.createElement('div');
+  elem.classList.add('results');
+  elem.textContent = 'Товар куплено!';
+  document.body.appendChild(elem);
+
+  const containerSecond = document.querySelector('.products');
+  const containerThird = document.querySelector('.details');
+  containerSecond.innerHTML='';
+  containerThird.innerHTML='';
+
+  const buttonClose = document.createElement('button');
+  buttonClose.classList.add('btnClose');
+  buttonClose.textContent = 'X';
+  elem.appendChild(buttonClose);
+
+  buttonClose.addEventListener('click', clickClose);
+}
+
+function clickClose(){
+  document.querySelector('.results').remove();
 }
 
 showCategories();
